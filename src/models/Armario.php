843860bbf18f48bbd245a16ddcef36177e2a649b
Kauã Lima <119634784+kauatwn@users.noreply.github.com>
2023-09-22 00:16:26 -0300

    Criando o modelo Armario

---
+++ src/models/Armario.php
@@ -0,0 +1,9 @@
+<?php
+
+/* Este arquivo é uma classe Model que deve representar a estrutura de dados do Administrador.
+Ela normalmente contém propriedades (atributos) que correspondem às colunas da tabela do banco de dados e pode incluir métodos para validação ou formatação.
+Os Models são frequentemente usados para mapear objetos do banco de dados para objetos na linguagem de programação. */
+
+class Armario
+{
+}
