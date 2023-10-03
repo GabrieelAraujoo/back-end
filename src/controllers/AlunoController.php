14fdef3a21f0a526d38ecdb8b722bdc90863ad9f
kauatwn <kaualopescontato@gmail.com>
2023-10-03 20:47:32 -0300

    Criando método de login

--- src/controllers/AlunoController.php
+++ src/controllers/AlunoController.php
@@ -3,6 +3,8 @@
 require_once(__DIR__ . '/../services/AlunoService.php');
 require_once(__DIR__ . '/../helpers/ResponseBuilder.php');
 require_once(__DIR__ . '/../models/Aluno.php');
+require_once(__DIR__ . '/../helpers/exceptions/ValidationException.php');
+require_once(__DIR__ . '/../helpers/LoginControllerUtil.php');
 
 /**
  * Controlador responsável por processar solicitações relacionadas ao cadastro de alunos.
@@ -49,11 +51,43 @@ class AlunoController
       $this->_alunoService->cadastrarAlunoService($aluno);
 
       // Retorna uma resposta JSON de sucesso.
-      return ResponseBuilder::successResponse('Aluno cadastrado com sucesso.');
+      return ResponseBuilder::successResponse(
+        "Aluno(a) cadastrado(a) com sucesso."
+      );
+    } catch (ValidationException $e) {
+
+      return ResponseBuilder::errorResponse(
+        "Erro de validação: {$e->getMessage()}"
+      );
     } catch (Exception $e) {
 
-      // Retorna uma resposta JSON de erro.
-      return ResponseBuilder::errorResponse('Erro de validação: ' . $e->getMessage());
+      return ResponseBuilder::errorResponse(
+        "Erro desconhecido: {$e->getMessage()}"
+      );
     }
+  }
+
+  /**
+   * Processa a tentativa de login de um aluno com base nos dados fornecidos.
+   *
+   * Verifica se a requisição é do tipo POST, recebe o e-mail e senha
+   * do formulário e tenta autenticar o aluno usando o serviço de autenticação.
+   * Em caso de sucesso, retorna uma resposta JSON de sucesso com código HTTP 200.
+   * Em caso de falha de validação, retorna uma resposta JSON de erro com código HTTP 401
+   * e uma mensagem de erro descritiva. Em caso de erro desconhecido, retorna uma resposta
+   * JSON de erro com código HTTP 500.
+   *
+   * @return string A resposta JSON que pode conter uma mensagem de sucesso ou erro.
+   */
+  public function processarLoginAluno()
+  {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      $email = $_POST['email'];
+      $senha = $_POST['senha'];
+
+      return LoginControllerUtil::processarLogin($email, $senha);
+    }
+    http_response_code(400);
+    return ResponseBuilder::errorResponse("Requisição inválida.");
   }
 }
