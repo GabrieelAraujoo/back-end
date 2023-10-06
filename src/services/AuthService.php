06dd788bb11677b45c75f40aeb4d82370111e93f
kauatwn <kaualopescontato@gmail.com>
2023-10-06 13:48:18 -0300

    comentários no auth

--- src/services/AuthService.php
+++ src/services/AuthService.php
@@ -25,25 +25,31 @@ class AuthService
    * @return bool Retorna true se o login for bem-sucedido para um aluno ou administrador, caso contrário, retorna false.
    * @throws ValidationException Lança uma exceção se o e-mail fornecido for inválido.
    */
-  public static function loginUser($email, $senha)
+  public static function login($email, $senha)
   {
-    // Verifique se o e-mail é válido usando FILTER_VALIDATE_EMAIL
+    // Verifique se o e-mail é válido.
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
       throw new ValidationException("E-mail inválido.");
     }
 
+    // Cria uma instância da classe DatabaseOperations.
     $_dbConnection = new DatabaseOperations();
 
     // Obtém um usuário com base no e-mail.
     $user = $_dbConnection->getOneUserByEmail($email);
 
+    // Verifica se o usuário existe e se a senha fornecida coincide com a senha armazenada no banco de dados.
     if ($user && password_verify($senha, $user['senha'])) {
+
       // Verifica se o tipo de usuário é 'aluno' ou 'admin'.
       if ($user['type'] === 'aluno' || $user['type'] === 'admin') {
+
         // Login bem-sucedido para aluno ou administrador.
         return true;
       }
     }
+
+    // Retorna falso se o usuário não estiver autenticado.
     return false;
   }
 }
