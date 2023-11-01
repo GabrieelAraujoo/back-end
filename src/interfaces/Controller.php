8848a481593f0b9753c23dfef555bbc60e7183e2
kauatwn <kaualopescontato@gmail.com>
2023-10-31 21:39:04 -0300

    Corrigindo a interface

--- src/interfaces/Controller.php
+++ src/interfaces/Controller.php
@@ -1,8 +1,35 @@
 <?php
 
+/**
+ * Interface para controladores.
+ */
 interface Controller
 {
+  /**
+   * Obtém todos os recursos.
+   *
+   * @return string Resposta JSON com todos os recursos.
+   */
   public function getAll();
+
+  /**
+   * Cria um novo recurso.
+   *
+   * @return string Resposta JSON com o resultado da criação do recurso.
+   */
   public function create();
-  public function login();
+
+  /**
+   * Atualiza um recurso.
+   *
+   * @return string Resposta JSON com o resultado da atualização do recurso.
+   */
+  public function update();
+
+  /**
+   * Exclui um recurso.
+   *
+   * @return string Resposta JSON com o resultado da exclusão do recurso.
+   */
+  public function delete();
 }
